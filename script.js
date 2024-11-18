369c8ea01b5c1d2760d7415b510d60c6cb55a694
Migueliank <miguel.iank@escola.pr.gov.br>
2024-11-18 11:09:40 -0300

    aula 2

---
+++ script.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(400, 400);
+    let palavra = prompt("Qual palavra deseja   codificar?"); 
+
+    background("#01080e");
+    console.log(palavra);
+
+    noLoop();
+  }
+    
